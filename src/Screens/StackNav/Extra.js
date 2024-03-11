9590c9b29d778324ad9c3b2590376cdabec257fa
Munna Kumar <munnakr686622@gmail.com>
2024-03-11 14:52:53 +0530

    update chnages

--- src/Screens/StackNav/Extra.js
+++ src/Screens/StackNav/Extra.js
@@ -21,7 +21,6 @@ import ContactAreaComp from '../../Components/ContactAreaComp';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { IP } from '../../Constants/Server';
-import Loader from '../../Components/Loader';
 
 const { width, height } = Dimensions.get('screen');
 
@@ -31,18 +30,17 @@ function Plans() {
   const [url, setUrl] = useState(null);
 
   const Data = [
-    { id: "price_1Ola6GGJyA6XB6N0nyCrs0iP", price: '13$/month', name: 'MONTHLY SUBSCRIPTION' },
-    { id: "price_1OlaAjGJyA6XB6N0wEiaH8Xr", price: '13$/month', name: '3 MONTH SUBSCRIPTION' },
+    { id: "price_1OAn62LnVrUYOeK2Y2M7l0Cj", price: '13$/month', name: 'MONTHLY SUBSCRIPTION' },
+    { id: "price_1OMYiBLnVrUYOeK2LPEbMEvW", price: '13$/month', name: '3 MONTH SUBSCRIPTION' },
   ];
 
   const handleSubmit = async (membership_id, index) => {
     console.log("membership_id", membership_id)
-    setLoading(true);
+    setLoading(index);
     try {
-      const user_id = await AsyncStorage.getItem('userid');
-      console.log("user_id id", user_id)
+      const user_id = "65ddb8bd879352c6c84dcb4d"
       const token = await AsyncStorage.getItem('token');
-      const url = `${IP}/payment/create-checkout-session?membership=${membership_id}&userId=${user_id}`;
+      const url = `http://45.13.132.197:5000/api/payment/create-checkout-session?membership=${membership_id}&userId=${user_id}`;
       const config = {
         headers: {
           'Content-Type': 'application/json',
@@ -54,10 +52,10 @@ function Plans() {
       console.log('Stripe Redirect URL:', url);
       setUrl(res.config.url);
       console.log('API Response:', res);
+      Linking.openURL(res.config.url); // Redirect user to Stripe checkout page
       if (res.config.url) {
-        setLoading(false);
         // Redirect to Stripe checkout page
-        Linking.openURL(res.config.url);
+        Linking.openURL(res.config.url); // Redirect user to Stripe checkout page
       } else {
         console.error('Invalid response from the server:', res);
       }
@@ -66,13 +64,10 @@ function Plans() {
       Alert.alert('API Error', error.message);
     } finally {
       if (loading === index) {
-        setLoading(false); // Reset loading state only if it matches the index
+        setLoading(null); // Reset loading state only if it matches the index
       }
     }
   };
-
-
-
 
   const Card = ({ item, index }) => {
     return (
@@ -92,28 +87,23 @@ function Plans() {
   }
 
   return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
-
-        <Header />
-        <View style={{ backgroundColor: Colors.mainColor, height: '100%', padding: 5 }}>
-          <View style={{ height: '10%', justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: responsiveFontSize(3), color: Colors.whiteText, fontWeight: '900' }}>PLANS</Text>
-          </View>
-          <ScrollView style={{ flex: 1, padding: 10, height: 'auto' }}>
-            <FlatList
-              data={Data}
-              renderItem={({ item, index }) => <Card item={item} index={index} />}
-              keyExtractor={item => item.id.toString()}
-              showsVerticalScrollIndicator={false}
-            />
-            <ContactAreaComp />
-          </ScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Header />
+      <View style={{ backgroundColor: Colors.mainColor, height: '100%', padding: 5 }}>
+        <View style={{ height: '10%', justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ fontSize: responsiveFontSize(3), color: Colors.whiteText, fontWeight: '900' }}>PLANS</Text>
         </View>
-      </SafeAreaView>
-      {loading ? <Loader /> : null}
-    </>
-
+        <ScrollView style={{ flex: 1, padding: 10, height: 'auto' }}>
+          <FlatList
+            data={Data}
+            renderItem={({ item, index }) => <Card item={item} index={index} />}
+            keyExtractor={item => item.id.toString()}
+            showsVerticalScrollIndicator={false}
+          />
+          <ContactAreaComp />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
